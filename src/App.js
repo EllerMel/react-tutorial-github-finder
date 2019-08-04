25d920d20cd0cd771b59b48ebe567c5821110fe0
M Eller <msmaedev@gmail.com>
2019-08-04 17:20:48 -0400

    Using class and hard coding data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component{
@@ -6,7 +8,10 @@ class App extends Component{
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Navbar title='Github Finder' icon='fa fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
